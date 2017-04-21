55b2d409738ef66d764574fa434f82e9303ce4d2
Jacques Bourquin <Jacques Bourquin>
2017-04-20 22:36:42 -0500

    Adding capacity and other stuff

--- NetworkAttack/edge.h
+++ NetworkAttack/edge.h
@@ -4,18 +4,32 @@
 
 class Edge
 {
+    const short MIN_CAPACITY = 1;
+    const short MAX_CAPACITY = 20;
     short m_id;
+    short m_capacity;
+    short m_flow;
+    bool m_visited;
     Node * m_source;
     Node * m_target;
     bool m_alive;
+    void calc_capacity();
 public:
     short id() { return m_id; }
+    short capacity() { return m_capacity; }
+    short flow() { return m_flow; }
+    void flow(short flow) { m_flow = flow; }
+    bool visited() { return m_visited; }
+    void visited(bool visited) { m_visited = visited; }
     Node source() { return Node(* m_source); }
     Node target() { return Node(* m_target); }
     bool alive() { return m_alive; }
     void alive(bool alive) { m_alive = alive; }
     Edge(short id, Node * source, Node * target)
-        : m_id(id), m_source(source), m_target(target), m_alive(0) { }
+        : m_id(id), m_source(source), m_target(target), m_alive(0)
+    {
+        calc_capacity();
+    }
     Edge(const Edge &);
 
 };
